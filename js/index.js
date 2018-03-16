8cb6869a46869e2f4c1ef6e6434df218e870a70e
antoni-minkiewicz <37368340+antoni-minkiewicz@users.noreply.github.com>
2018-03-16 19:29:11 +0000

    First website upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$("#WIM_WENDERS").hide();
+$("button").click(function(){
+    $("#bottom-right").fadeOut();
+    $("#WIM_WENDERS").fadeIn();
+      $("button").click(function(){
+        $("#bottom-right").fadeIn()});
+  });
